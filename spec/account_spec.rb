126aaea699c1ad05dcbb758e1859c3047aa7cc99
Kerry Buckley <kerryjbuckley@gmail.com>
2008-10-28 18:09:51 +0000

    Pending spec for debit account

---
+++ spec/account_spec.rb
@@ -0,0 +1,8 @@
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+require 'account'
+
+describe Account do
+  describe 'debiting' do
+    it 'should reduce the balance by the debit amount'
+  end
+end
