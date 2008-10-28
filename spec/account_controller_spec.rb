93ebad846a0f3bfa137bb356ed14236c8f9da319
Kerry Buckley <kerryjbuckley@gmail.com>
2008-10-28 18:06:24 +0000

    Passing spec for credit

--- spec/account_controller_spec.rb
+++ spec/account_controller_spec.rb
@@ -6,11 +6,16 @@ describe AccountController do
   describe 'transferring money' do
     it 'should debit the source account' do
       source = mock Account
-      dest = mock Account
+      dest = stub_everything Account
       source.should_receive(:debit).with 123
       AccountController.new.transfer source, dest, 123
     end
     
-    it 'should credit the destination account'
+    it 'should credit the destination account' do
+      source = stub_everything Account
+      dest = mock Account
+      dest.should_receive(:credit).with 123
+      AccountController.new.transfer source, dest, 123
+    end
   end
 end
