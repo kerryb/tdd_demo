7d0b8fb52838555e12293ee017d28a3578246a54
Kerry Buckley <kerryjbuckley@gmail.com>
2008-10-28 18:47:18 +0000

    Refactored account specs

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,19 +3,21 @@ require 'account'
 require 'spec'
 
 describe Account do
+  before do
+    @account = Account.new(100)
+  end
+  
   describe 'debiting' do
     it 'should reduce the balance by the debit amount' do
-      account = Account.new(100)
-      account.debit(10)
-      account.balance.should == 90
+      @account.debit(10)
+      @account.balance.should == 90
     end
   end
   
   describe 'crediting' do
     it 'should increase the balance by the credit amount' do
-      account = Account.new(100)
-      account.credit(10)
-      account.balance.should == 110
+      @account.credit(10)
+      @account.balance.should == 110
     end
   end
 end
