2be01af3c05bd938bfe5cd631abe87d7bc3c6d22
Kerry Buckley <kerryjbuckley@gmail.com>
2008-10-28 18:27:04 +0000

    Fixed steps (convert amounts to integers)

--- features/steps.rb
+++ features/steps.rb
@@ -5,13 +5,13 @@ require 'spec'
 
 Given /^an account called '(\w*)' containing £(\d*)$/ do |name, amount|
   @@accounts ||= {}
-  @@accounts[name] = Account.new(amount)
+  @@accounts[name] = Account.new(amount.to_i)
 end
 
 When /^I transfer £(\d*) from (\w*) to (\w*)$/ do |amount, from, to|
-  AccountController.new.transfer @@accounts[from], @@accounts[to], amount
+  AccountController.new.transfer @@accounts[from], @@accounts[to], amount.to_i
 end
 
 Then /^the '(\w*)' account should contain £(\d*)$/ do |name, amount|
-  @@accounts[name].balance.should == amount
+  @@accounts[name].balance.should == amount.to_i
 end
