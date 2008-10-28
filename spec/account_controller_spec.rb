7e1bae0e99d996fba43e53839d0fae45f4d05bfc
Kerry Buckley <kerryjbuckley@gmail.com>
2008-10-28 18:02:21 +0000

    Pending controller specs

--- spec/account_controller_spec.rb
+++ spec/account_controller_spec.rb
@@ -1,3 +1,9 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 require 'account_controller'
 
+describe AccountController do
+  describe 'transferring money' do
+    it 'should debit the source account'
+    it 'should credit the destination account'
+  end
+end
