a2b31e20101f84958b1d5d53a52c3f10bb3fd516
Kerry Buckley <kerryjbuckley@gmail.com>
2008-10-28 18:08:17 +0000

    Refactored controller specs

--- spec/account_controller_spec.rb
+++ spec/account_controller_spec.rb
@@ -4,18 +4,20 @@ require 'account'
 
 describe AccountController do
   describe 'transferring money' do
+    before do
+      @controller = AccountController.new
+      @source = stub_everything Account
+      @dest = stub_everything Account
+    end
+    
     it 'should debit the source account' do
-      source = mock Account
-      dest = stub_everything Account
-      source.should_receive(:debit).with 123
-      AccountController.new.transfer source, dest, 123
+      @source.should_receive(:debit).with 123
+      @controller.transfer @source, @dest, 123
     end
     
     it 'should credit the destination account' do
-      source = stub_everything Account
-      dest = mock Account
-      dest.should_receive(:credit).with 123
-      AccountController.new.transfer source, dest, 123
+      @dest.should_receive(:credit).with 123
+      @controller.transfer @source, @dest, 123
     end
   end
 end
